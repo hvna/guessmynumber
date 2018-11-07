48aef2a519bf72c5980c34702ca0d0c086b06cf3
hvna <44810739+hvna@users.noreply.github.com>
2018-11-07 09:40:33 -0700

    Add files via upload

---
+++ guessmynumber.py
@@ -0,0 +1,29 @@
+'''
+document
+'''
+
+import random
+
+secret = random.randint (1,99)  # generate random integers between 1 and 99
+
+guess = 0
+tries = 0
+print("Can you guess my number?")
+print("My number is an integer between 1 and 99")
+
+while guess != secret and tries < 6 :
+    print("What's your guess?")
+    guess = int (input())
+    if guess < secret :
+        print("Too low")
+    elif guess > secret :
+        print("Too high")
+
+    tries = tries + 1
+
+
+    if guess == secret :
+        print("You got it!")
+    else :
+        print("Nope! Try again")
+print("The secret number is secret = ", secret)
